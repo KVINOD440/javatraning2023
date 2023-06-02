6cf2b4cfd2de6dfa5b4f1286323664fb89aab1ee
Daring Developers <121381594+KVINOD440@users.noreply.github.com>
2023-06-02 17:54:04 +0530

    Add files via upload

---
+++ ascii.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class ascii
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+char n;
+n=sc.next().charAt(0);
+int ascci=(int)n;
+System.out.println("ASCII value of"+n+" is "+ascci);
+}
+}
